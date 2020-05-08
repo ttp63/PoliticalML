1271c800753f31cd99041ef9c1cc936f0c3bacf1
Ian McBride <imcbride63@gmail.com>
2020-05-08 16:54:54 -0500

    final commit for turn in deadline

--- Scripts/clean&analyze.py
+++ Scripts/clean&analyze.py
@@ -137,7 +137,7 @@ dropped = base_df[base_df["text"].isna() & base_df["urls"].isna()]
 # df for base model
 simple_df = base_df[base_df["text"].notna()]
 
-# Replace missing null values for dirty vs clean demostration
+# Replace missing null values for dirty vs clean demonstration
 ml_df["text"] = ml_df["text"].fillna(value=".")
 
 #%% SPACY SETUP
@@ -147,35 +147,17 @@ import en_core_web_sm
 from spacy.lang.en.stop_words import STOP_WORDS
 from spacy.lang.en import English
 
-from sklearn.base import TransformerMixin
-from sklearn.pipeline import Pipeline
-
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegressionCV
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.linear_model import LogisticRegression
-
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-
 nlp = en_core_web_sm.load()
 
 # Punctuation marks
 punctuations = string.punctuation
-
-# Add nan to STOP_WORDS
-# STOP_WORDS
 
 # Load English tokenizer, tagger, parser, NER and word vectors
 parser = English()
 
 # Creating our tokenizer function
 def spacy_tokenizer1(sentence):
-    # Creating our token object, which is used to create documents with linguistic annotations.
+    # Creating our token object
     mytokens = parser(sentence)
 
     # Lemmatizing each token and converting each token into lowercase
@@ -193,19 +175,6 @@ def spacy_tokenizer1(sentence):
     return mytokens
 
 
-# Custom transformer using spaCy
-class predictors(TransformerMixin):
-    def transform(self, X, **transform_params):
-        # Cleaning Text
-        return [clean_text(text) for text in X]
-
-    def fit(self, X, y=None, **fit_params):
-        return self
-
-    def get_params(self, deep=True):
-        return {}
-
-
 # Basic function to clean the text
 def clean_text(text):
     return text.strip().lower()
@@ -213,7 +182,6 @@ def clean_text(text):
 
 # Create spacy vectorizers
 spacy_cv1 = CountVectorizer(tokenizer=spacy_tokenizer1, ngram_range=(1, 1))
-
 
 #%% LETS FIT SOME MODELS
 test_classifier = MultinomialNB()
@@ -280,20 +248,33 @@ sentence = "Donald Trump visited Apple headquarters THIS IS a smaple as;odfi n s
 sentence2 = "THis is a test sentance to see how SPACY works White Collar espn.com"
 
 # %% MODEL TESTING
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import RidgeClassifier
+
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
 import time
 
-ngrams = [(1, 2)]  # (1, 1),
+ngrams = [(1, 1), (1, 2)]
 tokenizer_list = [
-    ["NTLK", nltk_tk_regexp],
-    ["NTLK_tweet", nltk_tk_tweet],
+    ["NLTK", nltk_tk_regexp],
+    ["NLTK_tweet", nltk_tk_tweet],
     ["SPACY", spacy_tokenizer1],
 ]
-model_list = [LogisticRegression(), LogisticRegressionCV()]
-rstates = [i for i in range(10, 21)]
-x_data = [["Clean", ml_df["clean_text"]]]  # ["Dirty", ml_df["text"]],
-vector_methods = ["Count"]  # , "TFIDF"]
+model_list = [LogisticRegression()]
+rstates = [i for i in range(16, 21)]
+x_data = [["Clean", ml_df["clean_text"]], ["Dirty", ml_df["text"]]]
+vector_methods = ["Count", "TFIDF"]
 
-results_name = "logistic_w_cv.csv"
+results_name = "logdetail.csv"
 results_list = []
 
 total_len = (
@@ -514,4 +495,157 @@ nbvlog_sum_df = (
     .reset_index()
 )
 
+logdetail_df = pd.read_csv("../Results/logdetail.csv")
+
+logdetail_sum_df = (
+    logdetail_df.groupby(["model", "tokenizer", "vectorizer", "ngram", "data_type"])
+    .agg(
+        {
+            "accuracy": "mean",
+            "null_accuracy": "mean",
+            "lift": "mean",
+            "dem_precision": "mean",
+            "rep_precision": "mean",
+            "dem_recall": "mean",
+            "rep_recall": "mean",
+        }
+    )
+    .reset_index()
+)
+
+logdetail_sum_df["model_num"] = range(1, len(logdetail_sum_df) + 1)
+logdetail_sum_df["model_name"] = (
+    logdetail_sum_df["model"].astype(str)
+    + "_"
+    + logdetail_sum_df["model_num"].astype(str)
+)
+
 # %%
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Classifier Comparison
+classax = class_sum_df.sort_values(by="lift").plot.barh(
+    x="model",
+    y="lift",
+    title="Basic Classifier Comparison",
+    xlim=(1, 1.6),
+    legend=False,
+)
+classax.set_ylabel("Classifier Type")
+classax.set_xlabel("Lift")
+
+plt.savefig("../Figures/class_comp.png", bbox_inches="tight")
+# All Logistic Models
+logdetailax = logdetail_sum_df.sort_values(by="lift").plot.barh(
+    x="model_num",
+    y="lift",
+    title="Logistic Model Parameter Tuning",
+    xlim=(1, 1.6),
+    legend=False,
+)
+logdetailax.set_ylabel("Model Number")
+logdetailax.set_xlabel("Lift")
+
+plt.savefig("../Figures/logdetail_comp.png", bbox_inches="tight")
+
+
+#%% BEST MODEL
+
+tokenizer_list = [["NLTK_tweet", nltk_tk_tweet]]
+rstates = [i for i in range(1, 20)]
+x_data = [["Clean", ml_df["clean_text"]]]
+y = ml_df["party"]
+tsize = 0.3
+best_results = []
+x = dataset[1]
+tokenizer = t[1]
+vector_type = "Count"  # Specify count, if not count defaults to TFIDF
+ngram = (1, 2)
+classifier = LogisticRegression()
+
+t0 = time.time()
+
+# Specify vectorizer
+if vector_type == "Count":
+    vect = CountVectorizer(tokenizer=tokenizer, lowercase=True, ngram_range=ngram)
+else:
+    vect = TfidfVectorizer(tokenizer=tokenizer, ngram_range=ngram)
+
+text_vect = vect.fit_transform(x)
+
+for r in rstates:
+    rstate = r
+
+    # Make new train test splits
+    x_train, x_test, y_train, y_test = train_test_split(
+        text_vect, y, test_size=tsize, random_state=rstate
+    )
+
+    model = classifier.fit(x_train, y_train)
+    prediction = model.predict(x_test)
+
+    null_prediction = y_test.replace(
+        to_replace=ml_df["party"].value_counts().sort_values().index[0],
+        value=ml_df["party"].value_counts().sort_values().index[1],
+    )
+
+    accuracy = metrics.accuracy_score(y_test, prediction)
+    dem_precision = metrics.precision_score(
+        y_test, prediction, pos_label="Democrat/Ind"
+    )
+    rep_precision = metrics.precision_score(y_test, prediction, pos_label="Republican")
+    dem_recall = metrics.recall_score(y_test, prediction, pos_label="Democrat/Ind")
+    rep_recall = metrics.recall_score(y_test, prediction, pos_label="Republican")
+    null_accuracy = metrics.accuracy_score(y_test, null_prediction)
+    lift = accuracy / null_accuracy
+
+    best_results.append(
+        [
+            str(classifier).split("(")[0],
+            t[0],
+            vector_type,
+            str(ngram),
+            rstate,
+            dataset[0],
+            accuracy,
+            dem_precision,
+            rep_precision,
+            dem_recall,
+            rep_recall,
+            null_accuracy,
+            lift,
+        ]
+    )
+
+t2 = time.time()
+print("Completed all models in " + str(round((t2 - t0), 2)) + " seconds.")
+
+best_results_df = pd.DataFrame(
+    results_list,
+    columns=[
+        "model",
+        "tokenizer",
+        "vectorizer",
+        "ngram",
+        "random_state",
+        "data_type",
+        "accuracy",
+        "dem_precision",
+        "rep_precision",
+        "dem_recall",
+        "rep_recall",
+        "null_accuracy",
+        "lift",
+    ],
+)
+
+# ELI5
+import eli5
+
+# Train best model on all data
+best_vect = CountVectorizer(tokenizer=nltk_tk_tweet, lowercase=True, ngram_range=(1, 2))
+best_text_vect = best_vect.fit_transform(ml_df["clean_text"])
+best_model = LogisticRegression().fit(best_text_vect, ml_df["party"])
+
+eli5.explain_weights(best_model, vec=best_vect, top=50)
